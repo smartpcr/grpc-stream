c369af9524a9145190115c1a6889254491b6f7fd
xiaodoli <xiaodoli@microsoft.com>
2020-07-01 16:38:12 -0700

    no quotes

--- Streaming.Web/scripts/protoc.ps1
+++ Streaming.Web/scripts/protoc.ps1
@@ -8,18 +8,18 @@ $grpcPlugin = Join-Path $nodeBinFolder "grpc_tools_node_protoc_plugin"
 $grpcTs = Join-Path $nodeBinFolder "protoc-gen-ts"
 
 
-$protocArgs = "$grpcProtoc --js_out=import_style=commonjs,binary:`"$protoFolder`" " +
-"--grpc_out=`"$protoFolder`" " +
-"--plugin=protoc-gen-grpc=`"$grpcPlugin`" " +
-"-I `"$protoFolder`" " +
-"`"$protoFolder/*.proto`""
+$protocArgs = "$grpcProtoc --js_out=import_style=commonjs,binary:$protoFolder " +
+"--grpc_out=$protoFolder " +
+"--plugin=protoc-gen-grpc=$grpcPlugin " +
+"-I $protoFolder " +
+"$protoFolder/*.proto"
 Write-Host $protocArgs
 Invoke-Expression $protocArgs
 
 $protocArgs = "$grpcProtoc " +
-"--plugin=protoc-gen-ts=`"$grpcTs`" " +
-"--ts_out=`"$protoFolder`" " +
-"-I `"$protoFolder`" " +
-"`"$protoFolder/*.proto`""
+"--plugin=protoc-gen-ts=$grpcTs " +
+"--ts_out=$protoFolder " +
+"-I $protoFolder " +
+"$protoFolder/*.proto"
 Write-Host $protocArgs
 Invoke-Expression $protocArgs
