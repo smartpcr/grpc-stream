6ddc9961f22b9d9df0ac8d349f7a6be3188f90ac
xiaodoli <xiaodoli@microsoft.com>
2020-07-05 03:59:52 -0700

    telemetry proto

--- web-client/protoc.sh
+++ web-client/protoc.sh
@@ -1,28 +1,34 @@
 #!/usr/bin/env bash
 
-PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 GRPC_TOOLS_NODE_PROTOC="./node_modules/.bin/grpc_tools_node_protoc"
+# GRPC_TOOLS_NODE_PROTOC="./node_modules/.bin/grpc_tools_node_protoc.cmd"
+PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 # PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts.cmd" # windows
 GRPC_TOOLS_NODE_PROTOC_PLUGIN="./node_modules/.bin/grpc_tools_node_protoc_plugin"
 
 PROTO_FOLDER="./src/proto/story"
-
 ${GRPC_TOOLS_NODE_PROTOC} \
   --js_out=import_style=commonjs,binary:"${PROTO_FOLDER}" \
   --grpc_out="${PROTO_FOLDER}" \
   --plugin=protoc-gen-grpc="${GRPC_TOOLS_NODE_PROTOC_PLUGIN}" \
   -I "${PROTO_FOLDER}" \
   "${PROTO_FOLDER}"/story.proto
-
 ${GRPC_TOOLS_NODE_PROTOC} \
   --plugin=protoc-gen-ts="${PROTOC_GEN_TS_PATH}" \
   --ts_out=service=true:"${PROTO_FOLDER}" \
   -I "${PROTO_FOLDER}" \
   "${PROTO_FOLDER}"/story.proto
 
-# ${GRPC_TOOLS_NODE_PROTOC} \
-#     --plugin=protoc-gen-ts="${PROTOC_GEN_TS_PATH}" \
-#     --ts_out=service=true:./src/proto/telemetry \
-#     --js_out=import_style=commonjs,binary:./src/proto/telemetry \
-#     -I ./src/proto/telemetry \
-#     ./src/proto/telemetry/temperature.proto
+
+PROTO_FOLDER="./src/proto/telemetry"
+${GRPC_TOOLS_NODE_PROTOC} \
+  --js_out=import_style=commonjs,binary:"${PROTO_FOLDER}" \
+  --grpc_out="${PROTO_FOLDER}" \
+  --plugin=protoc-gen-grpc="${GRPC_TOOLS_NODE_PROTOC_PLUGIN}" \
+  -I "${PROTO_FOLDER}" \
+  "${PROTO_FOLDER}"/story.proto
+${GRPC_TOOLS_NODE_PROTOC} \
+  --plugin=protoc-gen-ts="${PROTOC_GEN_TS_PATH}" \
+  --ts_out=service=true:"${PROTO_FOLDER}" \
+  -I "${PROTO_FOLDER}" \
+  "${PROTO_FOLDER}"/story.proto
